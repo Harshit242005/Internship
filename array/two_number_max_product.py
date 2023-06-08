df80100d31bddd4a3d9d229e72c9740f11eac7f9
Harshit242005 <112629431+Harshit242005@users.noreply.github.com>
2023-06-08 15:45:53 +0530

    Delete array directory

--- array/two_number_max_product.py
+++
@@ -1,9 +0,0 @@
-# in a given array find out the maximum product of two number 
-# by this you can find out the addition, subtraction, and divide
-def get_max_product(arr, size):
-    arr.sort()
-    return (arr[size] * arr[size-1])
-
-arr = [2, 1, 3, 0, 4]
-size = len(arr)-1
-print(get_max_product(arr, size))                   
